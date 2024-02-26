d5c0b339a922b347f963e0801dc2744936c97814
joabeArthur <131968361+joabeArthur@users.noreply.github.com>
2024-02-26 09:07:24 -0300

    projetoconfia

---
+++ MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using Microsoft.VisualBasic;
+
+namespace OprojetoConfia;
+
+public partial class MainPage : ContentPage
+{
+
+	String[] = new String[6];
+	List<String> frases = new List<String>()/
+
+	//string fraseGerada = null;
+	public MainPage()
+	{
+		InitializeComponent();
+
+		frases[0] = "boa noite faz o L!!!!";
+		frases[1] = "boa noite (o brigadeiro ta diferente)";
+		frases[2] = "boa noite IMORTAL";
+		frases[3] = "BOA NOITE Jair";
+		frases[4] = "uma noite Temperrada";
+		frases[5] = "boa noite gostosa";
+	
+	}
+
+	void GerarPalavraClicano()
+
+	
+	
+}
+
